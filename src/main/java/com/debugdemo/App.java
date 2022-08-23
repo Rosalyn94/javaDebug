75e97a7f0d21d044223164a8fea2fd7f354ad215
Ken Smith <kpsc59@umstem.edu>
2022-08-23 14:56:37 -0500

    update

--- src/main/java/com/debugdemo/App.java
+++ src/main/java/com/debugdemo/App.java
@@ -11,7 +11,7 @@ public class App
         int[] input = {1, 2, 3, 4, 5, 6, 7, 1, 2, 0};
 
         int sum = 0;
-        for (int i = 0; i < input.length; i++) {
+        for (int i = 0; i <= input.length; i++) {
             sum += 1 / input[i];            
         }
 
